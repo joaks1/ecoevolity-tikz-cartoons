a5e3dcf30d10b007f4c039e28b9dfdc2861407dc
Jamie R. Oaks <joaks1@gmail.com>
2021-10-26 12:36:09 -0500

    Adding code for new figures.
    Adding cartoon species trees with a gene treee embedded within.

--- generate-images.sh
+++ generate-images.sh
@@ -51,7 +51,40 @@ do
                 mkdir -p "$dir"
             fi
         fi
-    
+
+        pdflatex "--jobname=$jobname" "$texfile"
+        pdfcrop "$pdfpath" "$pdfpath"
+        pdf2svg "$pdfpath" "$svgpath"
+        if [ -n "$(echo "$jobname" | grep "island")" ]
+        then
+            convert -density 300 "$pdfpath" -flatten -strip -resize 1024 "$pngpath"
+            convert -density 600 -compress jpeg -quality 80 "$pdfpath" "$compressedpdfpath"
+        fi
+        rm "$logpath"
+    done
+done
+
+for texfile in sp-tree-*.tex
+do
+    for graphicline in $(grep "beginpgfgraphicnamed" "$texfile")
+    do
+        jobname="${graphicline#*\{}"
+        jobname="${jobname%*\}}"
+        echo $jobname
+        pdfpath="${jobname}.pdf"
+        compressedpdfpath="${jobname}-compressed.pdf"
+        svgpath="${jobname}.svg"
+        pngpath="${jobname}.png"
+        logpath="${jobname}.log"
+        dir="$(dirname $jobname)"
+        if [ -n "$dir" ]
+        then
+            if [ ! -e "$dir" ]
+            then
+                mkdir -p "$dir"
+            fi
+        fi
+
         pdflatex "--jobname=$jobname" "$texfile"
         pdfcrop "$pdfpath" "$pdfpath"
         pdf2svg "$pdfpath" "$svgpath"
